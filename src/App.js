a4f9fcf26fab6d725c93edf634927a233b2c4436
Savvin Michael <mihachess2004@gmail.com>
2023-07-23 16:13:55 +0300

    Some changes in data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Column from './Column';
 function App() {
   return (
     <div className="App">
-      <Column items = {[{name:'Изучить js',id:0},{name:'Изучить js',id:1},{name:'Изучить js',id:2},{name:'Изучить js',id:3}]} header="JS"/>
+      <Column items = {[{name:' Изучить js',id:0},{name:'Изучить js',id:1},{name:'Изучить js',id:2},{name:'Изучить js',id:3}]} header="JS"/>
       <Column items = {[{name:'Изучить js',id:0},{name:'Изучить js',id:1},{name:'Изучить js',id:2},{name:'Изучить js',id:3}]}  header="CSS"/>
        <Column items = {[{name:'Изучить js',id:0},{name:'Изучить js',id:1},{name:'Изучить js',id:2},{name:'Изучить js',id:3},{name:'Изучить js',id:4},{name:'Изучить js',id:5}]}  header="HTML"/>
       <AddColumn/>
